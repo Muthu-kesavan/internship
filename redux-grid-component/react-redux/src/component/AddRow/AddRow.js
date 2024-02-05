0fda2051147ec30ebdd9859e2429bc96a190129c
Muthu kesavan S <muthukesavan6044@gmail.com>
2024-02-05 09:45:42 +0530

    Comments

--- redux-grid-component/react-redux/src/component/AddRow/AddRow.js
+++ redux-grid-component/react-redux/src/component/AddRow/AddRow.js
@@ -4,16 +4,21 @@ import { addRow } from '../../redux/action';
 import '../AddRow/AddRow.css';
 const AddRow = ({ addRow }) => {
   const [rowData, setRowData] = useState('');
-
+// Task 1 is done here where our AddRow component dispacth an action
+// to update redux store state...
+// Task 2 is done here where we need to add new rows to the grid without making a server call or API refresh
   const handleAddRow = () => {
     if (rowData.trim() !== '') {
+// here it dispatches the addRow action with new data
       addRow(rowData);
+  // here it clears the input field after adding one 
       setRowData('');
     }
   };
 
   return (
     <div className='addrow'>
+
       <input
         type="text"
         value={rowData}
--- redux-grid-component/react-redux/src/component/Grid/Grid.js
+++ redux-grid-component/react-redux/src/component/Grid/Grid.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+//Task 2 is done here where we need to Use redux store and pass to child components
 const Grid = ({ gridData }) => {
   return (
     <div>
@@ -13,7 +13,8 @@ const Grid = ({ gridData }) => {
     </div>
   );
 };
-
+// hre mapStateToProps connects the Grid component to the Redux store
+// and maps the gridData from the store to the gridData prop.
 const mapStateToProps = (state) => {
   return {
     gridData: state.gridData,
