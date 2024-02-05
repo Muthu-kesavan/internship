1323f80ae0cca6e1b6a7002cb1faee4dc242b9e3
Muthu kesavan S <muthukesavan6044@gmail.com>
2024-02-05 07:01:51 +0530

    Updates

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -21,7 +21,14 @@ const Navbar = () => {
             <li><Link to="/pie-chart">Pie-Chart</Link></li>
             <li><Link to="/bubble-chart">Bubble-Chart</Link></li>
             <li><Link to="/scatter-plot">Scatter-Plot</Link></li>
-            <li><a href='https://www.linkedin.com/in/muthu-kesavan-s-95425821b/'> Profile</a></li>
+            <li><a href='https://www.linkedin.com/in/muthu-kesavan-s-95425821b/'
+                   target="_blank"
+                   rel="noopener noreferrer"> 
+                   Profile</a></li>
+            <li><a href='https://github.com/Muthu-kesavan/internship'  
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  Source Code</a></li>
           </ul>
         </li>
       </ul>
